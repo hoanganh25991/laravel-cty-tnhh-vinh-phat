9c9a72210423d7cb0816db3536a8b97644ba8596
Than Doan Dang Khoa <dangkhoa114@gmail.com>
2016-08-12 15:05:42 +0700

    home-page first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/fonts/flat-icon/flaticon.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300italic,300,400italic,500,500italic,700,700italic&subset=latin,vietnamese'
+          rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="container">
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Vĩnh Phát</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Trang chủ</a></li>
+                    <li><a href="#">Giới thiệu</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+    </nav>
+</div>
+<div class="jumbotron">
+    <div class="container">
+        <h1>Hello, world!</h1>
+
+        <p></p>
+
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+    </div>
+</div>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <div class="box">
+                    <div class="box-left">
+                        <span class="icon icon-md icon-primary flaticon-rain-weather-cloud-outline-symbol-with-raindrops-lines"></span>
+                    </div>
+                    <div class="box-body">
+                        <h3>Hoạt động số 1</h3>
+                    </div>
+                    <div class="description">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <div class="box">
+                    <div class="box-left">
+                        <span class="icon icon-md icon-primary flaticon-circular-outlined-badge-with-ribbon-recognition-prize-symbol"></span>
+                    </div>
+                    <div class="box-body">
+                        <h3>Hoạt động số 2</h3>
+                    </div>
+                    <div class="description">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <div class="box">
+                    <div class="box-left">
+                        <span class="icon icon-md icon-primary flaticon-foggy-day-fog-at-sunset-or-sunrise"></span>
+                    </div>
+                    <div class="box-body">
+                        <h3>Hoạt động số 3</h3>
+                    </div>
+                    <div class="description">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <div class="box">
+                    <div class="box-left">
+                        <span class="icon icon-md icon-primary flaticon-four-rounded-squares-button"></span>
+                    </div>
+                    <div class="box-body">
+                        <h3>Hoạt động số 4</h3>
+                    </div>
+                    <div class="description">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <div class="box">
+                    <div class="box-left">
+                        <span class="icon icon-md icon-primary flaticon-hot-interface-symbol-of-fire-flames-outline"></span>
+                    </div>
+                    <div class="box-body">
+                        <h3>Hoạt động số 5</h3>
+                    </div>
+                    <div class="description">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <div class="box">
+                    <div class="box-left">
+                        <span class="icon icon-md icon-primary flaticon-two-drops-sizes-outlined-shapes"></span>
+                    </div>
+                    <div class="box-body">
+                        <h3>Hoạt động số 6</h3>
+                    </div>
+                    <div class="description">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <div class="row"><h2 class="section-title">Our Products</h2></div>
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="img/product-1.jpg" alt="">
+                    <div class="caption">
+                        <h4>Product 1</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut
+                            labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="img/product-2.jpg" alt="">
+
+                    <div class="caption">
+                        <h4>Product 2</h4>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut
+                            labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="img/product-3.jpg" alt="">
+
+                    <div class="caption">
+                        <h4>Product 3</h4>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut
+                            labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
