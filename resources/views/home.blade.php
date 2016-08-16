e9b73c69d9a91afea83053cfd343c4cbfb16e4b8
Than Doan Dang Khoa <dangkhoa114@gmail.com>
2016-08-16 10:54:08 +0700

    Fix display on mobile. Now it looks much better when visit on mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/fonts/flat-icon/flaticon.css">
@@ -11,39 +12,37 @@
           rel='stylesheet' type='text/css'>
 </head>
 <body>
-<div class="container">
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Vĩnh Phát</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Trang chủ</a></li>
-                    <li><a href="#">Giới thiệu</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <form class="navbar-form navbar-left" role="search">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Search">
-                        </div>
-                    </form>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Vĩnh Phát</a>
         </div>
-    </nav>
-</div>
-<div class="jumbotron">
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Trang chủ</a></li>
+                <li><a href="#">Giới thiệu</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <form class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                </form>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+</nav>
+<section class="jumbotron">
     <div class="container">
         <h1>Hello, world!</h1>
 
@@ -51,11 +50,11 @@
 
         <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
     </div>
-</div>
+</section>
 <section>
     <div class="container">
         <div class="row">
-            <div class="col-xs-6 col-md-4">
+            <div class="col-xs-12 col-md-4">
                 <div class="box">
                     <div class="box-left">
                         <span class="icon icon-md icon-primary flaticon-rain-weather-cloud-outline-symbol-with-raindrops-lines"></span>
@@ -71,7 +70,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-6 col-md-4">
+            <div class="col-xs-12 col-md-4">
                 <div class="box">
                     <div class="box-left">
                         <span class="icon icon-md icon-primary flaticon-circular-outlined-badge-with-ribbon-recognition-prize-symbol"></span>
@@ -87,7 +86,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-6 col-md-4">
+            <div class="col-xs-12 col-md-4">
                 <div class="box">
                     <div class="box-left">
                         <span class="icon icon-md icon-primary flaticon-foggy-day-fog-at-sunset-or-sunrise"></span>
@@ -103,7 +102,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-6 col-md-4">
+            <div class="col-xs-12 col-md-4">
                 <div class="box">
                     <div class="box-left">
                         <span class="icon icon-md icon-primary flaticon-four-rounded-squares-button"></span>
@@ -119,7 +118,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-6 col-md-4">
+            <div class="col-xs-12 col-md-4">
                 <div class="box">
                     <div class="box-left">
                         <span class="icon icon-md icon-primary flaticon-hot-interface-symbol-of-fire-flames-outline"></span>
@@ -135,7 +134,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-6 col-md-4">
+            <div class="col-xs-12 col-md-4">
                 <div class="box">
                     <div class="box-left">
                         <span class="icon icon-md icon-primary flaticon-two-drops-sizes-outlined-shapes"></span>
@@ -198,39 +197,41 @@
         </div>
     </div>
 </section>
-<section>
-    <div class="row"><h2 class="section-title">Liên hệ</h2></div>
-    <div class="row">
-        <div class="container">
-            <div class="col-md-8">
-
-            </div>
-            <div class="col-md-8">
-                <form action="" class="form-horizontal">
-                    <div class="form-group">
-                        <label for="ho_va_ten" class="control-label">Họ và tên</label>
-                        <input type="text" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="message">Lời nhắn</label>
-                        <textarea name="message" id="" cols="30" rows="10" class="form-control"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-6">
-                            <div class="row">
-                                <label for="email">Email</label>
-                                <input type="text" class="form-control">
+<section class="message">
+    <div class="container">
+        <div class="row"><h2 class="section-title">Liên hệ</h2></div>
+        <div class="row">
+            <div class="container">
+                <div class="col-md-8">
+        
+                </div>
+                <div class="col-md-8">
+                    <form action="" class="form-horizontal">
+                        <div class="form-group">
+                            <label for="ho_va_ten" class="control-label">Họ và tên</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="message" class="control-label">Lời nhắn</label>
+                            <textarea name="message" id="" cols="30" rows="10" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <label for="email" class="control-label">Email</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-xs-12">
+                                <div class="row">
+                                    <label for="email" class="control-label">&nbsp;</label>
+                                    <button class="btn btn-primary btn-block">GỬI</button>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <label for="email">&nbsp;</label>
-                            <button class="btn btn-primary btn-block">Gửi</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="col-md-4">
-                <div class="contact-block">
+                    </form>
+                </div>
+                <div class="col-md-4 col-xs-12 contact-block">
                     <dl>
                         <dt>Địa chỉ</dt>
                         <dd>ABC</dd>
@@ -244,7 +245,6 @@
         </div>
     </div>
 </section>
-bs3-footer
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
